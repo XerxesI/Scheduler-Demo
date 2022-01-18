3ebcc6d7f43ae6674e7c4def306ac6aafcb14f1a
Meelis Kivimäe <meelis.kivimae@gmail.com>
2022-01-19 00:29:19 +0200

    Scheduler was merged with files reading from the directory

---
+++ src/main/java/com/progile/Scheduler/ListFiles.java
@@ -0,0 +1,21 @@
+package com.progile.Scheduler;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ListFiles {
+    public static Set<String> listFilesUsingFileWalk(String dir, int depth) throws IOException {
+        try (Stream<Path> stream = Files.walk(Paths.get(dir), depth)) {
+            return stream
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(Path::toAbsolutePath)
+                    .map(Object::toString)
+                    .collect(Collectors.toSet());
+        }
+    }
+}
--- src/main/java/com/progile/Scheduler/SchedulerDemoApplication.java
+++ src/main/java/com/progile/Scheduler/SchedulerDemoApplication.java
@@ -6,29 +6,42 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@EnableScheduling
 @Slf4j
 @SpringBootApplication
-@EnableScheduling
 public class SchedulerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerDemoApplication.class, args);
 	}
 
-	@Scheduled(cron="*/2 * * * * *")
-	public void job() throws InterruptedException {
-		log.info("Job current time"+ new Date());
-		Thread.sleep(1000L);
+	public static final int DEPTH = 1;
 
+	@Scheduled(cron="*/3 * * * * *")
+	public void TestDirectoryList() {
+//            System.out.println(PropertiesCache.getInstance().getProperty("filepath"));
+		log.info("Job start time "+new Date());
+
+		try {
+			String filePath = PropertiesCache.getInstance().getProperty("filepath");
+			Set<String> results = ListFiles.listFilesUsingFileWalk(filePath, 1);
+			results.stream().forEach(System.out::println);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Something went wrong.");
+		}
 	}
 
-	@Scheduled(cron="*/2 * * * * *")
-	public void job2() throws InterruptedException {
-		log.info("Job2 current time"+ new Date());
-		Thread.sleep(1000L);
 
-	}
 
 }
