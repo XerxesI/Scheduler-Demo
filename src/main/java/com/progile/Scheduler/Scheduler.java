23837ff0efed664a35a3a6b6a76c48e51c1e613c
Meelis Kivimäe <meelis.kivimae@gmail.com>
2022-05-18 21:59:44 +0300

    Sceduler logic was moved to the separate file Scheduler

--- src/main/java/com/progile/Scheduler/Scheduler.java
+++ src/main/java/com/progile/Scheduler/Scheduler.java
@@ -1,6 +1,5 @@
 package com.progile.Scheduler;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
